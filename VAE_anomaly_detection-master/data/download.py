ca8ff1fda505f2ec0d791631d728ced71f5a84e5
coxc25 <coxc25@wwu.edu>
2025-11-02 11:36:16 -0800

    Pushing code to pull data from opensearch

--- VAE_anomaly_detection-master/data/download.py
+++ VAE_anomaly_detection-master/data/download.py
@@ -1,33 +1,39 @@
-from opensearchpy import OpenSearch
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+from urllib3.exceptions import InsecureRequestWarning
+import urllib3
+import sys
 
-host = "140.160.1.66"
-port = 50001
+if len(sys.argv) != 3:
+    print("ERROR: USERNAME and PASSWORD not specified.")
+    sys.exit()
 
-# Create the client with SSL/TLS enabled, but hostname verification disabled.
-client = OpenSearch(
-    hosts = [{'host': host, 'port': port}],
-    http_compress = True, # enables gzip compression for request bodies
-    use_ssl = False,
-    verify_certs = False,
-    ssl_assert_hostname = False,
-    ssl_show_warn = False,
+username = sys.argv[1]
+password = sys.argv[2]
+
+# Suppress HTTPS warnings (because verify=False)
+urllib3.disable_warnings(InsecureRequestWarning)
+
+# OpenSearch URL
+url = "https://140.160.1.66:9200/_search"
+
+# Simple query: get first 10 documents from all indexes
+query = {
+    "query": { "match_all": {} },
+    "size": 10
+}
+
+# Send the request
+response = requests.get(
+    url,
+    headers={"Content-Type": "application/json"},
+    auth=HTTPBasicAuth(username, password),
+    data=json.dumps(query),
+    verify=False  # ignore SSL certificate
 )
 
-breakpoint()
-
-#query = {
-#  'query': {
-#        
-#    }
-#}
-
-
-
-response = client.search(
-    body = '*',
-    index = '*'
-)
 
 breakpoint()
-print(result)
-
+# Print the response
+print(response.text)
