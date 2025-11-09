6228d23316351a25d14f95ccad350547d5b8236e
EulerFrog <joescottewert@gmail.com>
2025-11-09 14:54:03 -0800

    removed deprecated code

---
+++ VAE_anomaly_detection-master/model/__init__.py
@@ -0,0 +1,5 @@
+"""VAE models for anomaly detection"""
+
+from .VAE import VAEAnomalyDetection, VAEAnomalyTabular, VAEAnomalyCNN
+
+__all__ = ['VAEAnomalyDetection', 'VAEAnomalyTabular', 'VAEAnomalyCNN']
