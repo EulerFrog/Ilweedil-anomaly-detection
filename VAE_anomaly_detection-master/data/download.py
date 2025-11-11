89ab7de4820fec75f472b4b906f5eecbcf525c4d
coxc25 <coxc25@wwu.edu>
2025-11-11 15:37:09 -0800

    created one hot encoding. and min max standardization.

--- VAE_anomaly_detection-master/data/download.py
+++ VAE_anomaly_detection-master/data/download.py
@@ -5,6 +5,7 @@ from urllib3.exceptions import InsecureRequestWarning
 import urllib3
 import sys
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
 
 def load_data(username, password):
     # Suppress HTTPS warnings (because verify=False)
@@ -48,7 +49,8 @@ def populate(raw_data, data):
         for col in columns:
             
             if col == "direction":
-                data[col].append(flow_dict["suricata"][col])
+                # 0 if to_server, 1 if to_client
+                data[col].append(0 if flow_dict["suricata"][col] == "to_server" else 1)
             elif col == "duration":
                 duration = (int(flow_dict["event"]["end"]) - int(flow_dict["event"]["start"])) / 1000 # convert from ms to sec
                 data[col].append(duration)
@@ -58,7 +60,6 @@ def populate(raw_data, data):
                 data[col].append(flow_dict[col][0])
             else:
                 data[col].append(flow_dict["suricata"]["flow"][col])
-
 
     return data
 
@@ -101,9 +102,38 @@ def remap(data):
         elif dest_port > 49151:
             data["dest_port"][idx] = "dynamic"
 
-    breakpoint()
-
     return data
+
+def one_hot(df):
+    categories = {
+        "src_port": ["HTTP", "HTTPS", "SSH", "DNS", "DHCP", "SMTP", "SNMP", "RDP", "SQL", "FTP", "public", "private", "dynamic"],
+        "dest_port": ["HTTP", "HTTPS", "SSH", "DNS", "DHCP", "SMTP", "SNMP", "RDP", "SQL", "FTP", "public", "private", "dynamic"],
+        "protocol": ["tcp", "udp", "icmp", "ipv6-icmp", "gre", "esp", "other"],
+    }
+
+    categorical_columns = df.select_dtypes(include=['object']).columns.tolist()
+    
+    # Create encoder with fixed categories and ignore unseen
+    encoder = OneHotEncoder(categories=[categories[col] for col in categorical_columns],
+                            handle_unknown='ignore', sparse_output=False)
+
+    # fit and transform list values to columns in the encoder
+    encoded = encoder.fit_transform(df[categorical_columns])
+    # create one hot encoded df (only categorical columns)
+    encoded_df = pd.DataFrame(encoded, columns=encoder.get_feature_names_out(categorical_columns))
+    # concat with original df
+    df_encoded = pd.concat([encoded_df, df], axis=1)
+    # delete columns that we don't want anymore (duplicates)
+    df_encoded = df_encoded.drop(categorical_columns, axis=1)
+    print(f"Encoded data : \n{df_encoded}")
+
+    return df_encoded
+
+def min_max(df, col):
+    col_min = df[col].min()
+    col_max = df[col].max()
+
+    return ((df[col] - col_min) / (col_max - col_min))
 
 def create_csvdata(data_dict):
     raw_data = data_dict["hits"]["hits"]
@@ -120,7 +150,7 @@ def create_csvdata(data_dict):
         "bytes_toclient": [],
         "pkts_toclient": [],
         "bytes_toserver": [],
-        "pkts_toclient": [],
+        "pkts_toserver": [],
         "direction": [],
         "duration": [],
         "protocol": [],
@@ -131,17 +161,51 @@ def create_csvdata(data_dict):
 
     data = populate(raw_data, data) 
 
-    breakpoint()
-    
-    # one hot encode ports, direction, protocol
     # remap port numbers to categories for one hot encoding
     data = remap(data)
 
+    # convert to pandas df
+    df = pd.DataFrame(data)
+
+    # one hot encode our dataframe
+    df = one_hot(df)
+
+#    categories = {
+#        "src_port": ["HTTP", "HTTPS", "SSH", "DNS", "DHCP", "SMTP", "SNMP", "RDP", "SQL", "FTP", "public", "private", "dynamic"],
+#        "dest_port": ["HTTP", "HTTPS", "SSH", "DNS", "DHCP", "SMTP", "SNMP", "RDP", "SQL", "FTP", "public", "private", "dynamic"],
+#        "protocol": ["tcp", "udp", "icmp", "ipv6-icmp", "gre", "esp", "other"],
+#    }
+#
+#    categorical_columns = df.select_dtypes(include=['object']).columns.tolist()
+#    
+#    # Create encoder with fixed categories and ignore unseen
+#    encoder = OneHotEncoder(categories=[categories[col] for col in categorical_columns],
+#                            handle_unknown='ignore', sparse_output=False)
+#
+#    # fit and transform list values to columns in the encoder
+#    encoded = encoder.fit_transform(df[categorical_columns])
+#    # create one hot encoded df (only categorical columns)
+#    encoded_df = pd.DataFrame(encoded, columns=encoder.get_feature_names_out(categorical_columns))
+#    # concat with original df
+#    df_encoded = pd.concat([encoded_df, df], axis=1)
+#    # delete columns that we don't want anymore (duplicates)
+#    df_encoded = df_encoded.drop(categorical_columns, axis=1)
+#    print(f"Encoded data : \n{df_encoded}")
+
+    breakpoint()
+    
+    breakpoint()
+    breakpoint()
     breakpoint()
 
-    df = pd.DataFrame(data)
+    numeric_cols = ["bytes_toclient", "bytes_toserver", "pkts_toclient", "pkts_toserver", "duration"]
     # standardize bytes, pkts, duration
-    [0-1]
+    for col in numeric_cols:
+        min_max(df, col)
+
+    breakpoint()
+    breakpoint()
+    breakpoint()
 
     # write csv
     df = pd.DataFrame(data)
