9f5a05b379f95e39889b1aa12076fe6e1ec87007
Juan Carlos Muratalla-Sanchez <murataj2@wwu.edu>
2025-11-12 20:49:33 -0800

    added dataset class, needs review

--- VAE_anomaly_detection-master/data/Dataset.py
+++ VAE_anomaly_detection-master/data/Dataset.py
@@ -11,9 +11,19 @@ class NetflowDatset(Dataset):
         # self.label = grab labels from data
         # steps: load_csv -> separate the last column into self.labels and the rest into self.data
         # throw some breakpoints for debugging
+        df = pd.read_csv(data_file)
+        self.data = df.iloc[:, :-1].values
+        self.labels = df.iloc[:, -1].values
+        print("Data loaded successfully")
+        print("Data shape:", self.data.shape)
+        print("Labels shape:", self.labels.shape)
 
     def __len__(self):
         # return len(self.data)
+        return len(self.data)
 
     def __getitem__(self, idx):
         # return self.data[idx], self.label[idx]
+        sample = torch.tensor(self.data[idx], dtype=torch.float32)
+        label = torch.tensor(self.labels[idx], dtype=torch.long)
+        return sample, label
